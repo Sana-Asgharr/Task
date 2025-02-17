dab4866107e449920198509500b596ffd45f3b87
Jahanzaib <jahanzaib0125@gmail.com>
2025-02-17 21:47:48 +0500

    conflict resolve

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-
-
+import { View, Text } from 'react-native'
 import React from 'react'
 import MainStackNavigator from './src/routers/StackNavigator'
-
-
+import { Provider } from 'react-redux';
+import { store } from './src/redux/Store';
+import Toast from 'react-native-toast-message';
+import { Button, Menu, Divider, PaperProvider } from 'react-native-paper';
 const App = () => {
   return (
-    <MainStackNavigator />
+    <PaperProvider>
+      <Provider store={store}>
+        <MainStackNavigator />
+      </Provider>
+      <Toast ref={ref => Toast.setRef(ref)} />
+    </PaperProvider>
   )
 }
 
